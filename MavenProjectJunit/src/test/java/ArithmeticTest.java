abbd174ebb3cb854fb297245d42043e5c79862e8
SohanGithubCSE <sohandatascience@gmail.com>
2025-07-03 02:15:37 +0530

    Update ArithmeticTest.java

--- MavenProjectJunit/src/test/java/ArithmeticTest.java
+++ MavenProjectJunit/src/test/java/ArithmeticTest.java
@@ -3,29 +3,29 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Arithmetic{
+public class ArithmeticTest{
 
     @Test
     public void testAddition() {
-        assertEquals(7, Arithmetic.add(2, 3));
-        assertEquals(3, Arithmetic.add(-2, 5));
+        assertEquals(7, ArithmeticTest.add(2, 3));
+        assertEquals(3, ArithmeticTest.add(-2, 5));
         
-        assertEquals(0, Arithmetic.add(0, 0));
+        assertEquals(0, ArithmeticTest.add(0, 0));
         
     }
 
     @Test
     public void testSubtraction() {
-        assertEquals(2, Arithmetic.subtract(5, 3));
-        assertEquals(-7, Arithmetic.subtract(-2, 5));
-        assertEquals(0, Arithmetic.subtract(0, 0));
+        assertEquals(2, ArithmeticTest.subtract(5, 3));
+        assertEquals(-7, ArithmeticTest.subtract(-2, 5));
+        assertEquals(0, ArithmeticTest.subtract(0, 0));
     }
 
     @Test
     public void testMultiplication() {
-        assertEquals(6, Arithmetic.multiply(2, 3));
-        assertEquals(-10, Arithmetic.multiply(-2, 5));
-        assertEquals(0, Arithmetic.multiply(0, 0));
+        assertEquals(6, ArithmeticTest.multiply(2, 3));
+        assertEquals(-10, ArithmeticTest.multiply(-2, 5));
+        assertEquals(0, ArithmeticTest.multiply(0, 0));
     }
     
     public static int add(int a,int b) {
